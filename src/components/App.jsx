372d0e6aa5ddc5e5d584a9e6ef1cd340d09a695c
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2022-12-25 14:43:53 +0200

    Refactors Feedback app to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,63 +1,73 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Section from './Section/Section.jsx';
 import Statistics from './Statistics/Statistics.jsx';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions.jsx';
 import Notification from './Notification/Notification';
 
-export class App extends Component {
-  state = {
+export const App = () => {
+  // We could craete three separate states for each conditions: good, bad and neutral
+  // pros: independent states and less render calls
+  // cons: three similiar functions, manual statistics handling
+
+  const initialStats = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
 
-  onFeedbackBtnClick = event => {
-    this.setState(prevState => {
-      return { [event.target.value]: prevState[event.target.value] + 1 };
+  const [stats, setStats] = useState(initialStats);
+
+  const onFeedbackBtnClick = event => {
+    setStats(prevState => {
+      return {
+        ...prevState,
+        [event.target.value]: prevState[event.target.value] + 1,
+      };
     });
   };
 
-  countTotalFeedback = () =>
-    Object.values(this.state).reduce((acc, el) => {
+  const countTotalFeedback = () =>
+    Object.values(stats).reduce((acc, el) => {
       return (acc += el);
     }, 0);
 
-  countPositiveFeedbackPercentage = () => {
-    if (this.state.good === 0) return '0%';
-    const avg = Math.floor((this.state.good / this.countTotalFeedback()) * 100);
-    return `${avg}%`;
+  const countPositiveFeedbackPercentage = () => {
+    // if (stats.good === 0) return '0%';
+    // const avg = Math.floor((state.good / countTotalFeedback()) * 100);
+    // return `${avg}%`;
+    return stats.good === 0
+      ? '0%'
+      : `${Math.floor((stats.good / countTotalFeedback()) * 100)}%`;
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
+  const { good, neutral, bad } = stats;
 
-    return (
-      <div style={{ padding: '1rem' }}>
-        <Section
-          title="Welcome to Expresso cafe"
-          motto="the best mix of an express train and a coffie drink"
-        >
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.onFeedbackBtnClick}
+  return (
+    <div style={{ padding: '1rem' }}>
+      <Section
+        title="Welcome to Expresso cafe"
+        motto="the best mix of an express train and a coffie drink"
+      >
+        <FeedbackOptions
+          options={Object.keys(stats)}
+          onLeaveFeedback={onFeedbackBtnClick}
+        />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback}
+            positivePercentage={countPositiveFeedbackPercentage}
           />
-        </Section>
-        <Section title="Statistics">
-          {this.countTotalFeedback() ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countTotalFeedback}
-              positivePercentage={this.countPositiveFeedbackPercentage}
-            />
-          ) : (
-            <Notification msg="There is no feedback" />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+        ) : (
+          <Notification msg="There is no feedback yet" />
+        )}
+      </Section>
+    </div>
+  );
+};
 
 export default App;
